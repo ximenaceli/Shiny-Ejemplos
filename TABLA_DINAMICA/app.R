cbcad1cf6bbbc23b21a719113a69d186be4b8267
Xime Celi <ximenaceli@gmail.com>
2018-05-12 13:48:34 -0500

    Ejemplo de tabla dinámica

---
+++ TABLA_DINAMICA/app.R
@@ -0,0 +1,23 @@
+library(shiny)
+library(rpivotTable)
+
+
+
+nacimientos2015 <- read_delim("nacimientos2015.csv", 
+                              ";", escape_double = FALSE, trim_ws = TRUE, locale = locale(encoding = "ISO-8859-1"))
+
+
+
+ui = fluidPage(
+  h1("Tabla dinámica Nacimientos", align="center"),
+  rpivotTableOutput("Tabla")
+)
+
+server = function(input, output) {
+  output$Tabla <- renderRpivotTable({
+    change_locale(rpivotTable(nacimientos2015),
+                  'es')
+  })
+}
+
+shinyApp(ui, server)
